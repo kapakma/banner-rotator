413fe6b2cca64843a44b701fe0dd3fc7e39dae97
Allan Ma <allankma@gmail.com>
2025-08-19 18:35:39 -0400

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,46 +1,44 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-import prettierPlugin from "eslint-plugin-prettier";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 export default defineConfig([
-  { 
-    files: ["**/*.{js,mjs,cjs}"], 
+  {
+    files: ['**/*.{js,mjs,cjs}'],
     ...js.configs.recommended,
     plugins: { prettier: prettierPlugin },
     languageOptions: {
       globals: {
         ...globals.browser,
-        $: "readonly",
-        jQuery: "readonly",
+        $: 'readonly',
+        jQuery: 'readonly',
       },
     },
-    extends: [
-      js.configs.recommended, 
-    ],
+    extends: [js.configs.recommended],
     rules: {
-      "no-console": "warn",
-      "no-alert": "error",
-      "eqeqeq": ["error", "always"],
-      "no-implicit-globals": "error",
-      "no-var": "error",
-      "prefer-template": "warn",
-      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-      "curly": ["error", "all"],
-      "dot-notation": "error",
-      "no-multi-spaces": "error",
-      "no-return-await": "error",
-      "yoda": "error",
-      "indent": ["error", 4],
-      "semi": ["error", "always"],
-      "comma-dangle": ["error", "always-multiline"],
-      "object-curly-spacing": ["error", "always"],
-      "array-bracket-spacing": ["error", "never"],
-      "key-spacing": ["error", { "beforeColon": false, "afterColon": true }],
-      "no-trailing-spaces": "error",
-      "eol-last": ["error", "always"],
-      "func-call-spacing": ["error", "never"],
-      "prettier/prettier": "error",
+      'no-console': 'warn',
+      'no-alert': 'error',
+      eqeqeq: ['error', 'always'],
+      'no-implicit-globals': 'error',
+      'no-var': 'error',
+      'prefer-template': 'warn',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      curly: ['error', 'all'],
+      'dot-notation': 'error',
+      'no-multi-spaces': 'error',
+      'no-return-await': 'error',
+      yoda: 'error',
+      indent: ['error', 4],
+      semi: ['error', 'always'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'object-curly-spacing': ['error', 'always'],
+      'array-bracket-spacing': ['error', 'never'],
+      'key-spacing': ['error', { beforeColon: false, afterColon: true }],
+      'no-trailing-spaces': 'error',
+      'eol-last': ['error', 'always'],
+      'func-call-spacing': ['error', 'never'],
+      'prettier/prettier': 'error',
     },
   },
 ]);
